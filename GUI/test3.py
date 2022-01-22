63719f25412092374ee5e6466f51fb73b45b708f
MaxEpstein <max.epstein@hotmail.com>
2022-01-22 12:16:40 -0500

    resized graph

--- GUI/test3.py
+++ GUI/test3.py
@@ -1,7 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
+from matplotlib.pyplot import figure 
 from matplotlib import animation
 import csv
+import matplotlib as mpl
+mpl.rcParams['toolbar'] = 'None'
 
 yArray = [[0]]
 xArray = [[0]]
@@ -11,22 +14,27 @@ with open('Flight_1063_C.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         if (i > 0):
-            print (row[1])
+            #print (row[1])
             yArray.append([row[2]])
             xArray.append([row[1]])
         i+=1
 
 
 fig, axis = plt.subplots()
+fig.set_size_inches(3, 3)
+axis.set_title('Altitude (M) Time (s)')
+axis.set_xlabel('time')
+axis.set_ylabel('altitude')
 line, = axis.plot([], [], '-ko')   #customize line (color, dots, etc)
 axis.margins(0.05)                  #makes it more fluid
+
 
 def init():                              #set first point
     line.set_data(xArray[:2],yArray[:2])
     return line,
 
 def animate(i):                         #draws line
-    win = 100                           #maximum window size
+    win = 20                           #maximum window size
     imin = min(max(0, i - win), len(xArray) - win)      #gets current window range
     xdata = xArray[imin:i]          #gets x values between range
     ydata = yArray[imin:i]          #gets y values between range
@@ -35,7 +43,7 @@ def animate(i):                         #draws line
     axis.autoscale()                   #renumbers x axis
     return line,
 
-anim = animation.FuncAnimation(fig, animate, init_func=init, interval=25)
+anim = animation.FuncAnimation(fig, animate, init_func=init, interval=1000)
 
 plt.show()
 
