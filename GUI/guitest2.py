8e052a3d9bd844e66762d2832241039df6ccb8dd
MaxEpstein <max.epstein@hotmail.com>
2022-01-22 19:27:32 -0500

    Changed data to variables

--- GUI/guitest2.py
+++ GUI/guitest2.py
@@ -1,6 +1,17 @@
 import PySimpleGUI as sg
 import time
 
+ID = 1063
+PT1 = 'C'
+PT2 = 'P'
+SS1 = 'LAUNCH_AWAITING'
+SS2 = 'LAUNCH_AWAITING'
+PC1 = 1584
+PC2 = 1687
+MODE = 'S'
+TP_DEPLOY = 'F'
+CMD_ECHO = 'CMD,1000,ST,13:35:59'
+
 def clock():
     return (time.strftime("%H:%M:%S", time.gmtime()))
 
@@ -10,18 +21,18 @@ top_banner = [[sg.Text('gSat ID: 1063', font='Any 26', background_color='#1B2838
                sg.Button('Connect', font='Any 16'),
                sg.Button('Close', font='Any 16')]]
 
-second_row = [[sg.Text('Packet Type 1: C', size=(14), font='Any 16', background_color='#1B2838'),
-               sg.Text('Mode: F', size=(7), font='Any 16', background_color='#1B2838'),
+second_row = [[sg.Text('Packet Type 1: '+ PT1, size=(14), font='Any 16', background_color='#1B2838'),
+               sg.Text('Mode: '+MODE, size=(7), font='Any 16', background_color='#1B2838'),
                sg.Text('GPS Time: ' + clock(), size=(18), font='Any 16', background_color='#1B2838', key='gpsTime'),
-               sg.Text('Software State 1: LAUNCH_AWAITING ', size=(32), font='Any 16', background_color='#1B2838'),
-               sg.Text('Packet Type 2: P', size=(14), font='Any 16', background_color='#1B2838'),
-               sg.Text('Software State 2: LAUNCH_AWAITING ', size=(32), font='Any 16', background_color='#1B2838')]]
+               sg.Text('Software State 1: '+SS1, size=(32), font='Any 16', background_color='#1B2838'),
+               sg.Text('Packet Type 2: ' + PT2, size=(14), font='Any 16', background_color='#1B2838'),
+               sg.Text('Software State 2: '+SS2, size=(32), font='Any 16', background_color='#1B2838')]]
 
-third_row = [[sg.Text('Packet Count 1: 1510', size=(17), font='Any 16', background_color='#1B2838'),
-               sg.Text('Tp Deploy: F', size=(10), font='Any 16', background_color='#1B2838'),
+third_row = [[sg.Text('Packet Count 1: '+str(PC1), size=(17), font='Any 16', background_color='#1B2838'),
+               sg.Text('Tp Deploy: '+TP_DEPLOY, size=(10), font='Any 16', background_color='#1B2838'),
                sg.Text('GPS Sat: 105', size=(13), font='Any 16', background_color='#1B2838'),
-               sg.Text('CMD Echo: CMD,1000,ST,13:35:59', size=(31), font='Any 16', background_color='#1B2838'),
-               sg.Text('Packet Count 2: 1698', size=(18), font='Any 16', background_color='#1B2838')]]
+               sg.Text('CMD Echo: '+CMD_ECHO, size=(31), font='Any 16', background_color='#1B2838'),
+               sg.Text('Packet Count 2: '+str(PC2), size=(18), font='Any 16', background_color='#1B2838')]]
 
 layout = [[top_banner],
           [second_row],
