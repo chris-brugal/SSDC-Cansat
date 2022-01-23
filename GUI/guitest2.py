5d8ad2004a46272d9a3913966c27a8528429178f
MaxEpstein <max.epstein@hotmail.com>
2022-01-22 19:29:19 -0500

    Fixed a spacing and variable issue

--- GUI/guitest2.py
+++ GUI/guitest2.py
@@ -15,7 +15,7 @@ CMD_ECHO = 'CMD,1000,ST,13:35:59'
 def clock():
     return (time.strftime("%H:%M:%S", time.gmtime()))
 
-top_banner = [[sg.Text('gSat ID: 1063', font='Any 26', background_color='#1B2838', border_width=(5), size=(40)),
+top_banner = [[sg.Text('gSat ID: '+str(ID), font='Any 26', background_color='#1B2838', border_width=(5), size=(40)),
                sg.Text(clock(), font='Any 22', background_color='#1B2838', key='time', border_width=(8), size=(10)),
                sg.Button('Calibrate', font='Any 16'),
                sg.Button('Connect', font='Any 16'),
@@ -29,7 +29,7 @@ second_row = [[sg.Text('Packet Type 1: '+ PT1, size=(14), font='Any 16', backgro
                sg.Text('Software State 2: '+SS2, size=(32), font='Any 16', background_color='#1B2838')]]
 
 third_row = [[sg.Text('Packet Count 1: '+str(PC1), size=(17), font='Any 16', background_color='#1B2838'),
-               sg.Text('Tp Deploy: '+TP_DEPLOY, size=(10), font='Any 16', background_color='#1B2838'),
+               sg.Text('TP Deploy: '+TP_DEPLOY, size=(11), font='Any 16', background_color='#1B2838'),
                sg.Text('GPS Sat: 105', size=(13), font='Any 16', background_color='#1B2838'),
                sg.Text('CMD Echo: '+CMD_ECHO, size=(31), font='Any 16', background_color='#1B2838'),
                sg.Text('Packet Count 2: '+str(PC2), size=(18), font='Any 16', background_color='#1B2838')]]
