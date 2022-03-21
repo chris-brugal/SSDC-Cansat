308cc7da7cad0f56449a86e0f85ea4afd6ca9e03
MaxEpstein <max.epstein@hotmail.com>
2022-03-21 19:29:35 -0400

    updated can and payoad chart update functions

--- GUI/guitest2.py
+++ GUI/guitest2.py
@@ -9,7 +9,8 @@ import pandas as pd
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 
-
+brown = '#854803'
+orange ='#f29411'
 
 _VARS = {'window': False,
          'fig_agg': False,
@@ -123,7 +124,7 @@ _VARS['window'] = sg.Window('test window', layout, margins=(0,0), location=(0,0)
 # <TEMP>, <VOLTAGE>, < GPS_TIME>, <GPS_LATITUDE>, <GPS_LONGITUDE>, <GPS_ALTITUDE>, <GPS_SATS>, 
 # <SOFTWARE_STATE>, <CMD_ECHO>
 
-def getData():  # get data from canister csv file
+def getCanData():  # get data from canister csv file
     data = pd.read_csv('Flight_1063_C.csv')
     global PC1 
     PC1 = data['PACKET_COUNT'][PC1 + 1]
@@ -140,7 +141,26 @@ def getData():  # get data from canister csv file
     gpsLONG = data['GPS_LONGITUDE']
     gpsALT = data['GPS_ALTITUDE']
     SS1 = data['SOFTWARE_STATE'][PC1]
-    return (PCC, alt, temp, volt, gpsLAT, gpsLONG, gpsALT, tPlus)
+    return (tPlus, alt, temp, volt, gpsLAT, gpsLONG, gpsALT)
+
+def getPayloadData():
+        data = pd.read_csv('Flight_5063_P.csv')
+        tPlusP = data['T+ Time']
+        altP = data['TP_ALTITUDE']
+        tempP = data['TP_TEMP']
+        voltP = data['TP_VOLTAGE']
+        gyroR = data['GYRO_R']
+        gyroP = data['GYRO_P']
+        gyroY = data['GYRO_Y']
+        accelR = data['ACCEL_R']
+        accelP = data['ACCEL_P']
+        accelY = data['ACCEL_Y']
+        magR = data['MAG_R']
+        magP = data['MAG_P']
+        magY = data['MAG_Y']
+        pe = data['POINTING_ERROR']
+        return(tPlusP, altP, tempP, voltP, gyroR, gyroP, gyroY, accelR,
+        accelP, accelY, magR, magP, magY, pe)
 
 def setyAxis():      #only done once in drawchart function to set axies
     _VARS['pltAxis0'].set_ylabel('Altitude')
@@ -200,13 +220,13 @@ def init():                              #set first point
 
 def animate(i):                       #draws line
 
-    dataXY = (getData)()
+    canData = (getCanData)()
     win = 7                           #maximum window size
-    imin = min(max(0, i - win), len(dataXY[0]) - win)      #gets current window range
-    xdata = dataXY[0][imin:i]          #gets x values between range
-    ydata = dataXY[1][imin:i]          #gets y values between range
-    gyroData = dataXY[2][imin:i]
-    tempData = dataXY[3][imin:i]
+    imin = min(max(0, i - win), len(canData[0]) - win)      #gets current window range
+    xdata = canData[0][imin:i]          #gets x values between range
+    ydata = canData[1][imin:i]          #gets y values between range
+    gyroData = canData[2][imin:i]
+    tempData = canData[3][imin:i]
     lines[0].set_data(xdata, ydata)     #sets line
     #lines[1].set_data(xdata, gyroData)
     #lines[2].set_data(xdata, tempData)
@@ -217,12 +237,12 @@ def animate(i):                       #draws line
                     #renumbers x axis
     return lines,
 
-x = 0
-while (x < 10):
-    anim = animation.FuncAnimation(_VARS['pltFig'+str(x)], animate, init_func=init, interval=1000)  #nees to be updated
-    x+=1
+#x = 0
+#while (x < 10):
+#    anim = animation.FuncAnimation(_VARS['pltFig'+str(x)], animate, init_func=init, interval=1000)  #nees to be updated
+#    x+=1
 
-plt.show()    #needs to be updated
+#plt.show()    #needs to be updated
 
 
 # Recreate Synthetic data, clear existing figre and redraw plot.
@@ -230,38 +250,90 @@ plt.show()    #needs to be updated
 #create clock to keep track of frame in order to update graph??
 #use panda to trigger new update in csv file and send to graph to update??
 
-def updateChart(start, end):   #THIS TAKES ALL DATA AND GRAPHS IT
+def updateCanChart(start, end):   #THIS TAKES ALL DATA AND GRAPHS IT
     #///////////////////////////////////////////////////////////////////////////////////////////////////////////
     #use t+ mission time instead of packet count since payload and canister are transmitting at different rates
     #so their graphs will will be moving faster/slower than the other????
     #///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    data = getData()
+    canData = getCanData()
+    canTPlus = canData[0][start:end]          #gets x values between range  THIS IS GETTING THE DATA AT PC1-7 to PC1 NOT THE ACTUAL VALUES
+    canAlt = canData[1][start:end]          #gets y values between range
+    canTemp = canData[2][start:end]
+    canVolt = canData[3][start:end]
+    gpsLat = canData[4][start:end]
+    gpsLong = canData[5][start:end]
+    gpsAlt = canData[6][start:end]
 
-    win = 7                           #maximum window size
-    imin = min(max(0, 10 - win), len(data[0]) - win)      #gets current window range
-    #this needs to get the data where pc1-7 to pc is located
-    pc = data[0][end-start]          #gets x values between range  THIS IS GETTING THE DATA AT PC1-7 to PC1 NOT THE ACTUAL VALUES
-    alt = data[1][end-start]          #gets y values between range
-    temp = data[2][end-start]
 
-    x = 0
-    while (x < 10):
-        _VARS['pltsubFig'+str(x)].plot(pc, alt, '-k')
-        _VARS['pltsubFig'+str(x)].plot(pc, temp, '-b')
-        x+=1
+    _VARS['pltsubFig0'].plot(canTPlus, canAlt, '-k')
+
+    _VARS['pltsubFig1'].plot(canTPlus, canTemp, '-k')
+
+    _VARS['pltsubFig2'].plot(canTPlus, canVolt, '-k')
+
+    _VARS['pltsubFig5'].plot(canTPlus, gpsLat, '-k')
+
+    _VARS['pltsubFig6'].plot(canTPlus, gpsLong, '-k')
+
+    _VARS['pltsubFig7'].plot(canTPlus, gpsAlt, '-k')
+
     i = 0
     while (i < 10):
         _VARS['pltAxis'+str(i)].relim()                       #renumbers x axis
         _VARS['pltAxis'+str(i)].autoscale()
         i+=1
 
+def updatePayloadChart(start, end):   #THIS TAKES ALL DATA AND GRAPHS IT
+
+    payloadData = getPayloadData()
+
+    payTPlus = payloadData[0][start:end]
+    payAlt = payloadData[1][start:end]
+    payTemp = payloadData[2][start:end]
+    payVolt = payloadData[3][start:end]
+    gyroR = payloadData[4][start:end] 
+    gyroP = payloadData[5][start:end] 
+    gyroY = payloadData[6][start:end] 
+    accelR = payloadData[7][start:end] 
+    accelP = payloadData[8][start:end] 
+    accelY = payloadData[9][start:end] 
+    magR = payloadData[10][start:end] 
+    magP = payloadData[11][start:end] 
+    magY = payloadData[12][start:end] 
+    pe = payloadData[13][start:end] 
+
+    _VARS['pltsubFig0'].plot(payTPlus, payAlt, '-r')
+
+    _VARS['pltsubFig1'].plot(payTPlus, payTemp, '-r')
+
+    _VARS['pltsubFig2'].plot(payTPlus, payVolt, '-r')
+
+    _VARS['pltsubFig3'].plot(payTPlus, gyroR, orange)
+    _VARS['pltsubFig3'].plot(payTPlus, gyroP, '-m')
+    _VARS['pltsubFig3'].plot(payTPlus, gyroY, brown)
+
+    _VARS['pltsubFig4'].plot(payTPlus, accelR, orange)
+    _VARS['pltsubFig4'].plot(payTPlus, accelP, '-m')
+    _VARS['pltsubFig4'].plot(payTPlus, accelY, brown)
+
+    _VARS['pltsubFig8'].plot(payTPlus, magR, orange)
+    _VARS['pltsubFig8'].plot(payTPlus, magP, '-m')
+    _VARS['pltsubFig8'].plot(payTPlus, magY, brown)
+
+    _VARS['pltAxis9'].plot(payTPlus, pe, '-r')
+
+
 # \\  -------- PYPLOT -------- //
 
 
 _VARS['window'].maximize()
 
-#updateChart(100,93)
+updateCanChart(0,7)
+updatePayloadChart(0,7)
+time.sleep(1)
+updateCanChart(1,8)
+updatePayloadChart(1,8)
 
 while True:
     event, values = _VARS['window'].read(timeout=10)
